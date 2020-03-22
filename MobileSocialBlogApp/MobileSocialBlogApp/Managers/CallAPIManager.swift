887737ef7188211af9acb043d58c79c1b1475945
Andre Insigne <andre.insigne.vc@gmail.com>
2020-03-23 07:01:25 +0800

    fix incoming and outgoing call flow

---
+++ MobileSocialBlogApp/MobileSocialBlogApp/Managers/CallAPIManager.swift
@@ -0,0 +1,13 @@
+//
+//  CallAPIManager.swift
+//  MobileSocialBlogApp
+//
+//  Created by Andre Insigne on 22/03/2020.
+//  Copyright © 2020 Andre Insigne. All rights reserved.
+//
+
+import UIKit
+
+class CallAPIManager: NSObject {
+
+}
---
+++ MobileSocialBlogApp/MobileSocialBlogApp/Models/CallRecords.swift
@@ -0,0 +1,13 @@
+//
+//  CallRecords.swift
+//  MobileSocialBlogApp
+//
+//  Created by Andre Insigne on 22/03/2020.
+//  Copyright © 2020 Andre Insigne. All rights reserved.
+//
+
+import UIKit
+
+class CallRecords: NSObject {
+
+}
